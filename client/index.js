2b9d48bada455f01032387ceeb2099cbb0d30246
khenryjoe <joseph.ketterer@gmail.com>
2020-11-02 20:45:05 +0000

    second

--- client/index.js
+++ client/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import SongList from "./components/SongList";
+import { Router, hashHistory, IndexRoute, Route } from "react-router";
+import App from "./components/App"
+import SongCreate from "./components/SongCreate"
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+        <Route path="songs/new" component={App}>
+        <IndexRoute component={SongCreate} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://jkette01:guesswho@cluster0.w0njc.mongodb.net/graphql?retryWrites=true&w=majority'
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
